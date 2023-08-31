0be00e9ddc5070b6c7970c8ced59388679d20d65
Demagaa <90135638+Demagaa@users.noreply.github.com>
2023-08-31 18:59:42 +0200

    Delete order-service/src/main/java/com/productmanager/orderservice directory

--- order-service/src/main/java/com/productmanager/orderservice/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.productmanager.orderservice.repository;
-
-import com.productmanager.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order,Long> {
-}
